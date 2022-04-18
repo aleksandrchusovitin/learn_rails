426610a27d7956b3b5df765fe9fe314e57278a0b
Aleksandr Chusovitin <aleksandr.chusovitin@dualbootpartners.com>
2022-04-19 00:04:17 +0700

    finish worked on blog

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,4 +1,7 @@
 class CommentsController < ApplicationController
+
+  http_basic_authenticate_with name: "dhh", password: "secret", only: :destroy
+
   def create
     @article = Article.find(params[:article_id])
     @comment = @article.comments.create(comment_params)
@@ -10,7 +13,7 @@ class CommentsController < ApplicationController
     @comment = @article.comments.find(params[:id])
     @comment.destroy
 
-    redirect_to article_path(@article), status: :see_other
+    redirect_to article_path(@article), status: 303
   end
 
   private
